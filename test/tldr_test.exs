f6191f107d74024beacdecca99c6029337e003cd
Eduardo Gurgel <eduardo@gurgel.me>
2014-04-14 19:30:55 +1200

    Use current os type first, and then seek for common

--- test/tldr_test.exs
+++ test/tldr_test.exs
@@ -11,9 +11,15 @@ defmodule TLDR.TLDRTest do
     assert capture_io(fn -> TLDR.main([]) end) =~ ~r/Usage:/
   end
 
-  test "call TLDR with one word argument" do
-    use_cassette "tldr_one_argument" do
+  test "call TLDR with a OS X non existing term" do
+    use_cassette "tldr_osx_non_existing_term" do
       assert capture_io(fn -> TLDR.main(["ls"]) end) =~ ~r/List all files with their rights, groups, owner/
+    end
+  end
+
+  test "call TLDR with a OS X existing term" do
+    use_cassette "tldr_osx_existing_term" do
+      assert capture_io(fn -> TLDR.main(["brew"]) end) =~ ~r/Package manager for OS X/
     end
   end
 end
